0741e42b45e83445f2bc36a64cbbbbe6c841dd58
André Victor <motoqueiroggamer@gmail.com>
2020-03-27 20:48:28 -0400

    Front-end da Aplicação?

---
+++ scripts.js
@@ -0,0 +1,17 @@
+function onOff() {
+    document
+        .querySelector("#modal")
+        .classList
+        .toggle("hide")
+}
+
+document
+                    .querySelector("button.fat")
+                    .addEventListener("click", function(){
+                        document
+                            .querySelector("footer")
+                            .classList
+                            .toggle("hide")
+                    })
+
+                    
